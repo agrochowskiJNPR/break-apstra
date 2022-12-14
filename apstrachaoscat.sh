50c6fdf8169374846741452400f81552ba6f297f
agrochowskiJNPR <100955679+agrochowskiJNPR@users.noreply.github.com>
2022-12-14 13:54:11 -0500

    Update apstrachaoscat.sh

--- apstrachaoscat.sh
+++ apstrachaoscat.sh
@@ -132,6 +132,19 @@ getswitchinfo
 echo "NB: This is a pretty bad hack, but should peg the cpu @100% on a vQFX. Hit ^C (Control-C) to stop the pain. Make certain that the Device System Health probe is enabled, and note also that it will take 6 minutes and 1 second to raise an anomaly"
 sshpass -proot123 ssh -o StrictHostKeyChecking=no root@$switch_ip 'dd if=/dev/zero of=/dev/null'
 }
+
+rebootall() {
+declare -A switches `curl -s -k --location --request POST "https://$apstraserver/api/blueprints/$bpid/qe?type=staging" --header "AUTHTOKEN: $authtoken" --header "Content-Type: application/json" --data-raw "{ \"query\": \"match(node('system', name='system', role=is_in(['leaf', 'access', 'spine', 'superspine'])))\"}" | jq -r '.items[].system | "switches" + "[" + .label + "]" + "=" + .system_id' |tr '\n' ' '`
+
+
+for dev in "${!switches[@]}";
+do 
+	switch_ip=`curl -k --location --request GET "https://$apstraserver/api/systems/${switches["$target"]}" --header "AUTHTOKEN: $authtoken" --data-raw "" | jq -r '.facts .mgmt_ipaddr'`;
+        ( echo 'request system reboot' echo 'yes' echo 'quit' ) | sshpass -proot123 ssh -o StrictHostKeyChecking=no root@"$switch_ip" "cli"
+done
+}
+
+
 TITLE="How Would You Like to Break Your Environment Today?"
 	
 items=(1 "Change Blueprint Name"
@@ -144,6 +157,7 @@ items=(1 "Change Blueprint Name"
        8 "Flap xe-0/0/0 on selected device"
        9 "Ramp a device CPU to raise device Health anomaly"
        10 "Run a Commit"
+       11 "reboot all junos devices under Apstra management"
        )
 
 while choice=$(dialog --title "$TITLE" \
@@ -161,6 +175,7 @@ while choice=$(dialog --title "$TITLE" \
 	8) flapif ; sleep 2 ;;
 	9) rampcpu ; ;; 
 	10) commitcurrent ; sleep 5 ;;
+	11) rebootall ; sleep 3 ;;
         *) ;; # some action on other
     esac
 done
